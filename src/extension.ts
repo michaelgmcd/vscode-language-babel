5018d16f8601772ac52671c095d59505d014d3aa
Michael McDermott <michael@mgmcdermott.com>
2018-07-15 16:44:13 -0400

    feature (#21): support auto-indenting

---
+++ src/extension.ts
@@ -0,0 +1,30 @@
+import { ExtensionContext, IndentAction, languages, LanguageConfiguration } from 'vscode';
+
+const wordPattern = /(-?\d*\.\d\w*)|([^\`\~\!\@\$\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\s]+)/g;
+
+export const jsxConfiguration: LanguageConfiguration = {
+  wordPattern,
+  onEnterRules: [
+    {
+      beforeText: /.*/,
+      afterText: /\/>/,
+      action: { indentAction: IndentAction.IndentOutdent },
+    }
+  ],
+};
+
+export const jsxAttrConfiguration: LanguageConfiguration = {
+  wordPattern,
+  onEnterRules: [
+    {
+      beforeText: />/,
+      afterText: /<\//,
+      action: { indentAction: IndentAction.IndentOutdent },
+    }
+  ],
+};
+
+export function activate(context: ExtensionContext) {
+  languages.setLanguageConfiguration('jsx', jsxConfiguration);
+  languages.setLanguageConfiguration('jsx-attr', jsxAttrConfiguration);
+}
