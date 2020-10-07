4ee5f0e35b750558e5aa54aa6df2bb322275415c
Michael McDermott (CORP) <michael.mcdermott@adp.com>
2020-10-07 10:30:34 -0400

    refactor: remove twitter plug

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,6 @@ import {
   IndentAction,
   languages,
   LanguageConfiguration,
-  window,
-  Uri,
-  env,
 } from 'vscode';
 
 const wordPattern = /(-?\d*\.\d\w*)|([^\`\~\!\@\$\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\s]+)/g;
@@ -32,22 +29,7 @@ export const jsxAttrConfiguration: LanguageConfiguration = {
   ],
 };
 
-const welcomeMessageShown = 'WELCOME_MESSAGE_SHOWN';
-
 export async function activate(context: ExtensionContext) {
   languages.setLanguageConfiguration('jsx', jsxConfiguration);
   languages.setLanguageConfiguration('jsx-attr', jsxAttrConfiguration);
-
-  if (!context.globalState.get<boolean>(welcomeMessageShown)) {
-    const response = await window.showInformationMessage(
-      'Thanks for downloading Babel JavaScript! Make sure to follow me on Twitter @michaelgmcd.',
-      'Follow',
-    );
-    if (response) {
-      env.openExternal(
-        Uri.parse('https://twitter.com/intent/user?screen_name=michaelgmcd'),
-      );
-    }
-    await context.globalState.update(welcomeMessageShown, true);
-  }
 }
